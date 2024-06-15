705edd4fcff542eb6eec49e5fe96a818018ea0ea
Bonifacio <mikaelbonifacio@gmail.com>
2024-06-14 23:50:59 -0300

    Add envio de dados

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        String nomeOriginal = "Lets code";
-        saudacao(nomeOriginal);
+        int resultado = soma(2, 3);
+        System.out.println(resultado);
     }
-    public static void saudacao(String nomeParametro) {
-        System.out.println("Hello, " + nomeParametro);
+    public static int soma(int a, int b) {
+        return a + b;
     }
 }
