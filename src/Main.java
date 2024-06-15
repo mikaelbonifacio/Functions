a2f923504c7e843ef681bfcb6131e87cf45eab01
Bonifacio <mikaelbonifacio@gmail.com>
2024-06-14 23:44:55 -0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Lets code";
+        saudacao(nome);
+    }
+    public static void saudacao(String nome) {
+        System.out.println("Hello, " + nome);
+    }
+
+
+}
