d300085fb9b5a40d7cc1ef3a6c89cdf21ce253c8
Bonifacio <mikaelbonifacio@gmail.com>
2024-06-14 23:46:43 -0300

    Separando variaveis

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        String nome = "Lets code";
-        saudacao(nome);
+        String nomeOriginal = "Lets code";
+        saudacao(nomeOriginal);
     }
-    public static void saudacao(String nome) {
-        System.out.println("Hello, " + nome);
+    public static void saudacao(String nomeParametro) {
+        System.out.println("Hello, " + nomeParametro);
     }
-
-
 }
